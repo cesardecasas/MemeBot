516f2e8e4f70e979a1080627957e81347cdd9588
Cesar De Casas <cesar.decasasgarcia@gmail.com>
2021-12-07 15:52:43 -0600

    deployment ready bot

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const {Client, Intents } = require('discord.js');
+const {Client, Intents, MessageAttachment } = require('discord.js');
 const intents = new Intents()
 intents.add(Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MESSAGES);
 const client = new Client({ intents: intents})
+const RedditImageFetcher = require("reddit-image-fetcher");
+
 require('dotenv').config()
 
  const Token = process.env.TOKEN
 
- console.log(client)
+
 
 client.on('ready', () => {
  console.log(`Logged in as ${client.user.tag}!`);
@@ -16,8 +18,43 @@ client.on('message', msg => {
  if (msg.content === 'ping') {
  msg.reply('pong');
  }
+
+ if(msg.content.includes('hola') || msg.content.includes('hi')){
+     console.log(msg)
+     msg.reply(`welcome ${msg.author.username}`)
+     
+ }
+
+ if(msg.content === '-meme'){
+    RedditImageFetcher.fetch({
+        type: 'meme'
+    }).then(result => {
+    
+        const file = new MessageAttachment(result[0]?.image);
+    
+        msg.reply({ files: [file] });
+    });
+ }
+
+ if(msg.content.includes('-custom')){
+     let sub = msg.content.split('-custom ')[1]
+     console.log(sub)
+     RedditImageFetcher.fetch({
+        type: 'custom',
+        subreddit: [`${sub}`]
+    }).then(result => {
+    
+        const file = new MessageAttachment(result[0]?.image);
+    
+        msg.reply({ files: [file] });
+    }).catch(err=>{
+        console.log(err)
+        msg.reply('invalid subreddit name ')
+    })
+ }
  });
 
-client.on('')
+
+
 
 client.login(Token);
